ac913e8974ba9a26450ea96d4587c3c752ed4220
GabrielRom1 <gabriel.rome787@gmail.com>
2025-02-03 21:53:58 -0400

    run on public ip network

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ main = Blueprint('main', __name__)
 
 #only run the app if main is execute
 if __name__ == '__main__':
-    app.run(debug = True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
+
 
 
 
